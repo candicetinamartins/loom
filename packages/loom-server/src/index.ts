ea3978566277d61ee4e60db739ff24e5fd0b862f
candice martins <candicetinamartins@users.noreply.github.com>
2026-04-08 23:37:23 +0200

    chore: export all Loom Server v7 services from index.ts

--- packages/loom-server/src/index.ts
+++ packages/loom-server/src/index.ts
@@ -1,2 +1,9 @@
 export { SharedGraphServer, type ServerConfig, type GraphQueryRequest, type GraphQueryResponse } from './SharedGraphServer'
 export { createServer } from './cli'
+
+// Loom Server v7 Services
+export { AgentSessionManager, type SessionConfig, type ManagedSession } from './AgentSessionManager'
+export { ProviderRouter, type Provider, type ProviderConfig, type RouteRequest, type RouteResponse } from './ProviderRouter'
+export { RateLimitManager, type RateLimitConfig, type TokenBucket } from './RateLimitManager'
+export { ResultAggregator, type AgentResult, type AggregatedResult } from './ResultAggregator'
+export { MCPClientPool, type MCPServerConfig, type MCPConnection } from './MCPClientPool'
